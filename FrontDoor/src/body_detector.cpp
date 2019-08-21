27452dec06892f38f8551037fa4d2f844a92f306
Jorge V. M. M <joevzq@gmail.com>
2019-08-21 07:54:05 -0500

    Write body_detector ctor and dtor.
    * Comment out other methods for procedural testing.

---
+++ FrontDoor/src/body_detector.cpp
@@ -0,0 +1,11 @@
+#include "body_detector.hpp"
+
+namespace hcv
+{
+	BodyDetector::BodyDetector() {}
+
+	BodyDetector::~BodyDetector() {}
+
+	//bool BodyDetector::DetectBody(const cv::Mat& i_img) {}
+
+} // namespace hcv
