7dd48af030a2d5a02a6048363c5b7246700fb761
Jorge V. M. M <joevzq@gmail.com>
2019-08-22 13:14:15 -0500

    Initial tests/ commit.

---
+++ FrontDoor/tests/fd_system_unittests.cc
@@ -0,0 +1,16 @@
+#include "fd_builder.hpp"
+#include "fd_system.hpp"
+#include <gtest/gtest.h>
+
+namespace {
+
+
+} // namespace
+
+
+int main(int argc, char **argv)
+{
+	testing::InitGoogleTest(&argc, argv);
+	
+	return RUN_ALL_TESTS();
+}
