e09de82061bd4e792a84c16cd232cbce51f47173
Jorge V. M. M <joevzq@gmail.com>
2019-10-04 08:41:29 -0500

    Connect to MongoDB.

--- telegram-bot/app.py
+++ telegram-bot/app.py
@@ -1,1 +1,21 @@
+import sys
+import os
+import logging
 
+import pymongo
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+mongoclient = MongoClient(os.environ['MONGO_URL'])
+
+# Check if connection to mongodb is authorized
+try:
+    mongoclient.admin.command('ismaster')
+except ConnectionFailure:
+    logging.exception('Connection to mongodb not authorized. Check your credentials in docker-compose.yml')
+    sys.exit(1)
+
+logging.info('Connection with mongodb established.')
+hausdb = mongoclient.hauscv
