3e05f9a308f276306eba9eb2bbdd90228ce35414
Jorge V. M. M <joevzq@gmail.com>
2019-10-04 08:43:34 -0500

    Basic unknown commands handler.

--- telegram-bot/app.py
+++ telegram-bot/app.py
@@ -59,6 +59,12 @@ def start(update, context):
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+
 
 logging.info('Polling now for new messages.')
 
